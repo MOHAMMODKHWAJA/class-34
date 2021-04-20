c641f7f8a1705f3ace6c2212b8447ceda8c428a1
MOHAMMODKHWAJA <75531003+MOHAMMODKHWAJA@users.noreply.github.com>
2021-04-20 16:51:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var ball;
+var ball1,database,position;
+var ball1position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    
+    console.log(database);
+   
+    ball1 =createSprite(250,250,10,10);
+    ball1.shapeColor = "green";
+
+ball1position=database.ref('ball/position');
+ball1position.on("value",readPosition,showError);
+
+
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+'x':position.x + x,
+'y':position.y + y,
+    })
+
+}
+
+function readPosition(data){
+position=data.val();
+console.log(position.x);
+ball1.x=position.x;
+ball1.y=position.y;
+
+}
+
+
+function showError(){
+console.log("there is an error");
+
+
+
+
+}
+
